4edebfcedc8c3da46673cf74f948d7bfb27f02a9
Pm Pranta <heyiampm@gmail.com>
2026-06-03 20:22:30 +0600

    3941. Password Strength

--- LeetCode-Solution/3941. Password Strength/3941.py
+++ LeetCode-Solution/3941. Password Strength/3941.py
@@ -3,26 +3,15 @@ class Solution:
         uniqueChars = set(password)
         totalStrength = 0
         
-        
-        has_lower = False
-        has_upper = False
-        has_digit = False
-        has_special = False
-        
         special_chars = {"!", "@", "#", "$"}
-        
         for char in uniqueChars:
-            if char.islower() and not has_lower:
+            if char.islower():
                 totalStrength += 1
-                has_lower = True
-            elif char.isupper() and not has_upper:
+            elif char.isupper():
                 totalStrength += 2
-                has_upper = True
-            elif char.isdigit() and not has_digit:
+            elif char.isdigit():
                 totalStrength += 3
-                has_digit = True
-            elif char in special_chars and not has_special:
+            elif char in special_chars:
                 totalStrength += 5
-                has_special = True
                 
         return totalStrength
