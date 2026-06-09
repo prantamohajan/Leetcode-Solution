55aa0becf81d9a1359e0680372a0863f00e641e4
Pm Pranta <heyiampm@gmail.com>
2026-06-09 16:18:22 +0600

    LeetCode Problem Solve

---
+++ LeetCode-Solution/2161. Partition Array According to Given Pivot/2161.py
@@ -0,0 +1,15 @@
+class Solution:
+    def pivotArray(self, nums: list[int], pivot: int) -> list[int]:
+        less_than_pivot = []
+        equal_to_pivot = []
+        greater_than_pivot = []
+
+        for num in nums:
+            if num < pivot:
+                less_than_pivot.append(num)
+            elif num == pivot:
+                equal_to_pivot.append(num)
+            else:
+                greater_than_pivot.append(num)
+
+        return less_than_pivot + equal_to_pivot + greater_than_pivot
