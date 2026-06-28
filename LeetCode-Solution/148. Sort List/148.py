a901977dbd83c2698674421e78898b18951306b3
Pm Pranta <heyiampm@gmail.com>
2026-06-28 21:13:44 +0600

    LeetCode-Solution\148. Sort List

---
+++ LeetCode-Solution/148. Sort List/148.py
@@ -0,0 +1,17 @@
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return None
+        
+        nodes = []
+        while head:
+            nodes.append(head)
+            head = head.next
+            
+        nodes.sort(key=lambda x: x.val)
+        
+        for i in range(len(nodes) - 1):
+            nodes[i].next = nodes[i + 1]
+        nodes[-1].next = None
+        
+        return nodes[0]
